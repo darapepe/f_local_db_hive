745feaf6f8814aa8e036c6dd0e4aa69dde24aad3
Darwin Perez Perez <perez.darwin@gmail.com>
2022-08-04 20:37:13 -0500

    solucion ejercicio

--- lib/data/datasources/local/user_local_datasource_hive.dart
+++ lib/data/datasources/local/user_local_datasource_hive.dart
@@ -7,6 +7,12 @@ import '../../models/userdb.dart';
 
 class UserLocalDataSourceHive {
   addUser(RandomUser user) {
+    Hive.box('users').add(UserDB(
+        gender: user.gender,
+        name: user.name,
+        country: user.city,
+        email: user.email,
+        picture: user.picture));
     // aquí se debe llamar Hive box (con el nombre de la caja) add con una instancia de UserDB
   }
 
@@ -24,15 +30,25 @@ class UserLocalDataSourceHive {
 
   deleteAll() async {
     logInfo("Deleting all from database");
+    return Hive.box('users').clear();
     // aquí se debe llamar Hive box (con el nombre de la caja) clear
   }
 
   deleteUser(index) async {
+    return Hive.box('users').deleteAt(index);
     // aquí se debe llamar Hive box (con el nombre de la caja) deleteAt usando el indice
   }
 
   updateUser(RandomUser user) async {
     logInfo("Updating entry $user");
+    return Hive.box('users').putAt(
+        user.id!,
+        UserDB(
+            gender: user.gender,
+            name: user.name,
+            country: user.city,
+            email: user.email,
+            picture: user.picture));
     // aquí se debe llamar Hive box (con el nombre de la caja) putAt usando el id como referencia y mandando un UserDB
   }
 }
